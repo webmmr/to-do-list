2a47079594b7be4e0b3065dfe83a27012c1e39e8
webmmr <mdmizanur3950@gmail.com>
2023-07-18 21:05:22 +0600

    Project Initialized

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const initialData = [
+  {
+    id: 1,
+    task: "Start the React Project",
+    status: true,
+  },
+  {
+    id: 2,
+    task: "Exercise For 1 hour",
+    status: false,
+  },
+  {
+    id: 3,
+    task: "Go to Super Shop for groceris",
+    status: true,
+  },
+  {
+    id: 4,
+    task: "Post a sale post for monitor",
+    status: false,
+  },
+  {
+    id: 5,
+    task: "Meditate for 10 minute",
+    status: false,
+  },
+  {
+    id: 6,
+    task: "Read for 1 hour",
+    status: false,
+  },
+  {
+    id: 7,
+    task: "Watch tv-series for 30 minute",
+    status: false,
+  },
+];
+
+export default initialData;
